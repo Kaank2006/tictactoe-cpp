d6159f4028b774271fd2f70dab6064d3c609f9a5
kaankucuk2006 <kaankucuk2006@gmail.com>
2021-06-26 14:17:10 +0300

    fixing segfault indicator, and a little tidying

--- main.cpp
+++ main.cpp
@@ -4,8 +4,8 @@
 
 using namespace std;
 
-void getBoard(const vector<vector<char>>& board, const int& size_x, const int& size_y);
-vector<vector<char>> ask(vector<vector<char>> &board, const string& player, const char& symbol);
+void getBoard(const vector<vector<char>>& board, const int& size_y, const int& size_x);
+vector<vector<char>> ask(vector<vector<char>> &board, const string& player, const char& symbol, const int& size_x, const int& size_y);
 bool check(vector<vector<char>> board, const string& p1, const string& p2);
 
 int main() {
@@ -29,19 +29,15 @@ int main() {
     getBoard(board, size_y, size_x);
 
     do {
-        ask(board, p1, 'X');
 
-        cout << "Current board:" << endl;
+        ask(board, p1, 'X', size_y, size_x);
         getBoard(board, size_y, size_x);
-
 
         if(check(board, p1, p2)){
             return EXIT_SUCCESS;
         }
 
-        ask(board, p2, 'O');
-
-        cout << "Current board:" << endl;
+        ask(board, p2, 'O', size_y, size_x);
         getBoard(board, size_y, size_x);
 
     } while (!check(board, p1, p2));
@@ -49,22 +45,23 @@ int main() {
     return EXIT_SUCCESS;
 }
 
-vector<vector<char>> ask(vector<vector<char>> &board, const string& player, const char& symbol) {
+vector<vector<char>> ask(vector<vector<char>> &board, const string& player, const char& symbol, const int& size_x, const int& size_y) {
     int row, column;
 
     cout << player << " (x), Where would you like to place? (Enter column and row):" << endl;
 
     cin >> column;
     cin >> row;
-    if (column < 1 or row < 1 or column > 3 or row > 3){
+    if (column < 1 or row < 1 or column > size_x or row > size_y){
         cout << "Ha ha nice try, you can't segfault me" << endl; // one of my last fixes, my mental health had degraded too much at this point
     }
     if (board[--row][--column] != '#'){
         cout << player << " tried to overwrite a sector.";
-        ask(board, player, symbol);
+        ask(board, player, symbol, size_y, size_x);
     }
 
     board[row][column] = symbol; // line 57 already decreases it. Optimisation 1000 /s
+    cout << "Current board:" << endl;
 
     return board;
 }
